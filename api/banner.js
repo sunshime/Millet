6b31128681a4c52d8e65ccc4138dbf963ba5171a
lfj <liao_fengji@163.com>
2020-03-26 18:29:31 +0800

    实现首页轮播图部分

--- api/banner.js
+++ api/banner.js
@@ -1,21 +1,19 @@
-const banner = {
-  bannerList: [{
-      imgurl: "/assets/images/banner1.jpg",
-      id: 1
-    },
-    {
-      imgurl: "/assets/images/banner2.jpg",
-      id: 2
-    },
-    {
-      imgurl: "/assets/images/banner3.jpg",
-      id: 3
-    },
-    {
-      imgurl: "/assets/images/banner4.jpg",
-      id: 4
-    }
-  ]
-}
+const bannerList = [{
+    imgurl: "/assets/images/banner1.jpg",
+    id: 1
+  },
+  {
+    imgurl: "/assets/images/banner2.jpg",
+    id: 2
+  },
+  {
+    imgurl: "/assets/images/banner3.jpg",
+    id: 3
+  },
+  {
+    imgurl: "/assets/images/banner4.jpg",
+    id: 4
+  }
+]
 
-export default banner;
+export default bannerList;
