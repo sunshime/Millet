a0063d55a613a2ba17cc1a1e7364a93c9fe70f10
lfj <liao_fengji@163.com>
2020-03-27 11:20:45 +0800

    实现首页

--- api/navList.js
+++ api/navList.js
@@ -1,42 +1,42 @@
 const navList = [{
-    url: '/assets/images/phone.png',
-    word: '手机'
+  icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0099822e42b4428cb25c4cdebc6ca53d.jpg',
+    title: '手机'
   },
   {
-    url: '/assets/images/tv.png',
-    word: '电视'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/ef6b4e9b9151849b3b1fb1dbf069c6ed.jpg',
+    title: '电视'
   },
   {
-    url: '/assets/images/pc.png',
-    word: '电脑'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/cd2aa2dcad6440b469c22e27db9b6236.jpg',
+    title: '电脑'
   },
   {
-    url: '/assets/images/intelligent.png',
-    word: '智能'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/dccc32d8e36685485fa89e0f2a580975.png?thumb=1&w=160&h=110&f=webp&q=90',
+    title: '智能'
   },
   {
-    url: '/assets/images/news.png',
-    word: '新品'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/9eb0178e3bfeb7d170edd641fb4be4bc.jpg',
+    title: '配件'
   },
   {
-    url: '/assets/images/phone.png',
-    word: '手机'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/b67de5a8ce9025fb5d8bac66e019d1c3.jpg?w=468&h=600',
+    title: '家电'
   },
   {
-    url: '/assets/images/tv.png',
-    word: '电视'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/aa1e0a4860cf40d34e1192d86691c82c.png?thumb=1&w=160&h=110&f=webp&q=90',
+    title: '路由器'
   },
   {
-    url: '/assets/images/pc.png',
-    word: '电脑'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/50649d7b5706f4cd9f658e93db6b6564.jpg',
+    title: '耳机'
   },
   {
-    url: '/assets/images/intelligent.png',
-    word: '智能'
+    icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/8c55361386a46857f962e5142baeefcf.png?thumb=1&w=40&h=40&f=webp&q=90',
+    title: '电源'
   },
   {
-    url: '/assets/images/news.png',
-    word: '新品'
+    icon: 'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/50180b158690a40f9cbc684c7646d297.jpg?thumb=1&w=40&h=40',
+    title: '出行'
   }
 ]
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,16 @@
 //index.js
 import bannerList from '../../api/banner.js'
-console.log('bannerList====>', bannerList)
+import navList from '../../api/navList.js'
+import goodList from '../../api/goodList.js'
+console.log('goodList====>', goodList)
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    swiperList: bannerList
+    swiperList: bannerList,
+    navList: navList,
+    goodList: goodList
   },
 
   onLoad: function() {},
