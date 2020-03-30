6b72ca5d95190140eb2b76eb0b32e058ce2c0cdd
lfj <liao_fengji@163.com>
2020-03-30 11:37:54 +0800

    实现发现部分

--- api/findData.js
+++ api/findData.js
@@ -1,42 +1,52 @@
 const findData = {
   navData: [{
-      navUrl: '',
-      imgSrc: '/assets/images/findH.png',
-      text: '新品预约'
-    }, {
-      navUrl: '',
-      imgSrc: '/assets/images/findA.png',
-      text: '活动频道'
-    }, {
-      navUrl: '',
-      imgSrc: '/assets/images/findS.png',
-      text: '线下门店'
-    }
-  ],
+    navUrl: '',
+    imgSrc: '/assets/images/findH.png',
+    text: '新品预约'
+  }, {
+    navUrl: '',
+    imgSrc: '/assets/images/findA.png',
+    text: '活动频道'
+  }, {
+    navUrl: '',
+    imgSrc: '/assets/images/findS.png',
+    text: '线下门店'
+  }],
 
-  subjectsData: [{
-      subjectUrl: '',
-      subjectSrc: '/assets/images/findSO.jpg',
-      text: '你所向往的智慧生活，小米为你实现',
-      num: '3'
+  contentData: [{
+      imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a8dd25cab48c60fc6387b9001eddc3f9.jpg',
+      videoSrc: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/eadb8ddc86f1791154442a928b042e2f.mp4',
+      text: '小米10发布会',
+      desc: ''
     },
     {
-      subjectUrl: '',
-      subjectSrc: '/assets/images/findSO.jpg',
-      text: '你所向往的智慧生活，小米为你实现',
-      num: '20'
+      imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/b4004fc968de331f702585d58b15aba2.jpg',
+      videoSrc: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7f49c1ccd75f76ec86b52c9ae4c4a082.mp4',
+      text: '小米MIX Alpha 开箱视频',
+      desc: ''
     },
     {
-      subjectUrl: '',
-      subjectSrc: '/assets/images/findSO.jpg',
-      text: '你所向往的智慧生活，小米为你实现',
-      num: '15'
+      imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/395fdedf0f40206032397e33beed756e.jpg',
+      videoSrc: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/1443581f88b733750c7763b2b3d67c52.mp4',
+      text: '小米5G新品手机发布会',
+      desc: ''
     },
     {
-      subjectUrl: '',
-      subjectSrc: '/assets/images/findSO.jpg',
-      text: '你所向往的智慧生活，小米为你实现',
-      num: ''
+      imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/9c37f64bada7b0b2758f50e50077fe6c.jpg',
+      videoSrc: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/efea1979661bac0d1704d761e5e89ded.mp4',
+      text: '小米电视5 创新背后的故事',
+      desc: ''
+    }, {
+      imgUrl: 'https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/bfc860e9877a601f9707006d31ddbd67.jpg',
+      videoSrc: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f2661b967a319f358fabe822aed708e6.mp4',
+      text: '小米手环4',
+      desc: '改变从今开始'
+    },
+    {
+      imgUrl: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/2fd26bb99b723337a2f8eaba84f7d5bb.jpg',
+      videoSrc: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e25d81c4922fca5ebe51877717ef9b76.mp4',
+      text: '小米10 8K手机拍大片',
+      desc: ''
     }
   ]
 }
--- pages/find/find.js
+++ pages/find/find.js
@@ -1,66 +1,93 @@
 // pages/my/my.js
+
+import findData from '../../api/findData.js'
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    navList: findData.navData,
+    contentList: findData.contentData,
+    videoTitle: "",
+    videoUrl: "",
+    isShow: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  close() {
+    console.log('关闭弹框。。。。')
+    let isShow = false;
+    this.setData({
+      isShow: isShow
+    })
+
+    console.log(this.data.isShow)
+  },
+  openVideo(e) {
+    console.log("e=====>", e)
+    let text = e.currentTarget.dataset.text;
+    let videosrc = e.currentTarget.dataset.videosrc;
+    this.setData({
+      videoTitle: text,
+      videoUrl: videosrc,
+      isShow: true
+    })
   }
 })
