a0c4d9dcdf853f33ab58f1e86e7828f528a01c3e
lfj <liao_fengji@163.com>
2020-03-31 17:06:43 +0800

    实现我的模块页面

--- api/banner.js
+++ api/banner.js
@@ -1,18 +1,21 @@
 const bannerList = [{
-    imgurl: "/assets/images/banner1.jpg",
+    imgurl: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/3c409123c70dc6fa2250916cb1cf2a4b.jpg?w=2452&h=920",
     id: 1
   },
   {
-    imgurl: "/assets/images/banner2.jpg",
+    imgurl: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/755aca9487082e7698e16f17cfb70839.jpg?w=2452&h=920",
     id: 2
   },
   {
-    imgurl: "/assets/images/banner3.jpg",
+    imgurl: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/4e28deb0c44f459de6ee2c27fa2f8c99.jpg?w=2452&h=920",
     id: 3
   },
   {
-    imgurl: "/assets/images/banner4.jpg",
+    imgurl: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e25025f1ebb643fcf7f35aae636aa630.jpg?w=2452&h=920",
     id: 4
+  }, {
+    imgurl: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/aecf3fae491c814759f726a5347c176e.jpg?w=2452&h=920",
+    id: 5
   }
 ]
 
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,4 +1,27 @@
 Page({
- data: {
- },
+  data: {
+    myList: {
+      list: [{
+          icon: '../../assets/images/findH.png',
+          text: "我的收藏"
+        },
+        {
+          icon: '../../assets/images/order.png',
+          text: "我的订单"
+        },
+        {
+          icon: '../../assets/images/phone.png',
+          text: "我的预约"
+        }
+      ],
+      card: {
+        icon: '../../assets/images/coupon.png',
+        text: "我的卡券包"
+      },
+      setting: {
+        icon: '../../assets/images/setting.png',
+        text: '设置'
+      }
+    }
+  },
 })
