2e3a88daf5a867ec1ae06cca1718014ecb75712e
lfj <liao_fengji@163.com>
2020-03-31 10:15:26 +0800

    实现购物车页面左滑删除功能（删除操作还未实现）

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -7,7 +7,6 @@ Page({
   data: {
     dataList: [{
         id: 1,
-        right: 0,
         img: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/50649d7b5706f4cd9f658e93db6b6564.jpg?thumb=1&w=200&h=200&f=webp&q=90",
         title: "小米真无线蓝牙耳机 Air 2",
         price: 278,
@@ -17,7 +16,6 @@ Page({
       },
       {
         id: 2,
-        right: 0,
         img: "https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/321610e787393c42e5cb2e5730a7681d.jpg?thumb=1&w=200&h=200",
         title: "小米小爱蓝牙音箱 随身版",
         price: 49,
@@ -27,7 +25,6 @@ Page({
       },
       {
         id: 3,
-        right: 0,
         img: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/1f32af53d1ad60f4980146f6a2b81019.jpg?thumb=1&w=200&h=200&f=webp&q=90",
         title: "小米无线充电宝青春版10000mAh",
         price: 129,
@@ -37,7 +34,6 @@ Page({
       },
       {
         id: 4,
-        right: 0,
         img: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c149537ebb0c2cdb977ddd93200074f4.jpg?thumb=1&w=200&h=200&f=webp&q=90",
         title: "小米旅行箱 20英寸 布朗熊限量版",
         price: 149,
@@ -48,117 +44,87 @@ Page({
     ],
     noCheck: "https://i.loli.net/2020/03/05/zaBLDGlk2dCUwZQ.png",
     check: "https://i.loli.net/2020/03/05/2ODunK73vlaHhIw.png",
-    startX: 0, //开始坐标
-    startY: 0,
     isChecked: false, // 是否全选
     checkList: [], // 选中的项
-    buyPrice: 0 // 结算总价
+    buyPrice: 0, // 结算总价
+
+    // 设置开始的位置
+    startX: 0,
+    startY: 0,
   },
 
   // 开始滑动
   touchStart(e) {
     console.log('touchStart=====>', e);
-    //开始触摸时 重置所有删除
-
-    this.data.dataList.forEach(function (v, i) {
-
-      if (v.isTouchMove) //只操作为true的
-
-        v.isTouchMove = false;
-
-    })
-
+    let dataList = [...this.data.dataList]
+    dataList.forEach(item => {
+      if (item.isTouchMove) {
+        item.isTouchMove = !item.isTouchMove;
+      }
+    });
     this.setData({
-
-      startX: e.changedTouches[0].clientX,
-
-      startY: e.changedTouches[0].clientY,
-
-      dataList: this.data.dataList
-
+      dataList: dataList,
+      startX: e.touches[0].clientX,
+      startY: e.touches[0].clientY
     })
   },
 
   touchMove(e) {
     console.log('touchMove=====>', e);
-    var that = this,
+    let moveX = e.changedTouches[0].clientX;
+    let moveY = e.changedTouches[0].clientY;
+    let indexs = e.currentTarget.dataset.index;
+    let dataList = [...this.data.dataList]
 
-      index = e.currentTarget.dataset.index, //当前索引
+    console.log('moveX====>', this.data.startX, '--', moveX);
+    console.log('moveY====>', this.data.startX, '--', moveY);
+    console.log('indexs====>', indexs);
 
-      startX = that.data.startX, //开始X坐标
 
-      startY = that.data.startY, //开始Y坐标
+    let angle = this.angle({
+      X: this.data.startX,
+      Y: this.data.startY
+    }, {
+      X: moveX,
+      Y: moveY
+    });
+    console.log('angle====>', angle);
 
-      touchMoveX = e.changedTouches[0].clientX, //滑动变化坐标
-
-      touchMoveY = e.changedTouches[0].clientY, //滑动变化坐标
-
-      //获取滑动角度
-
-      angle = that.angle({
-        X: startX,
-        Y: startY
-      }, {
-        X: touchMoveX,
-        Y: touchMoveY
-      });
-
-    that.data.dataList.forEach(function (v, i) {
-
-      v.isTouchMove = false
-
-      //滑动超过30度角 return
-
-      if (Math.abs(angle) > 30) return;
-
-      if (i == index) {
-
-        if (touchMoveX > startX) //右滑
-
-          v.isTouchMove = false
-
-        else //左滑
-
-          v.isTouchMove = true
-
+    dataList.forEach((item, index) => {
+      item.isTouchMove = false;
+      if (angle > 30) {
+        return
       }
 
+      if (indexs === index) {
+        if (moveX > this.data.startX) { // 右滑
+          item.isTouchMove = false;
+        } else { // 左滑
+          item.isTouchMove = true;
+        }
+
+        console.log('item===>',item.isTouchMove);
+        
+      }
     })
-
-    //更新数据
-
-    that.setData({
-
-      dataList: that.data.dataList
-
+    console.log('dataList===>',dataList);
+    
+    this.setData({
+      dataList: dataList
     })
+    console.log('this.data.dataList====>', this.data.dataList);
 
   },
-  touchEnd(e) {
-    console.log('touchEnd=====>', e);
 
-  },
   /**
-
-* 计算滑动角度
-
-* @param {Object} start 起点坐标
-
-* @param {Object} end 终点坐标
-
-*/
-
+   * 计算滑动角度
+   * @param {Object} start 起点坐标
+   * @param {Object} end 终点坐标
+   */
   angle: function (start, end) {
-
     var _X = end.X - start.X,
-
       _Y = end.Y - start.Y
-
     //返回角度 /Math.atan()返回数字的反正切值
-
     return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
-
   },
-
-
 })
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,126 +1,78 @@
-// pages/leftSwiperDel/index.js
-
-import delList from '../../api/delList.js'
-
-var app = getApp();
+var app = getApp()
 Page({
-
-  data: {
-    delWidth: 180,
-    list: [{
-        id: 0,
-        name: '张三',
-        tel: 13552525252,
-        address: '河北省石家庄市长安区',
-        style: ''
-      },
-      {
-        id: 1,
-        name: '李四',
-        tel: 13552525252,
-        address: '河北省石家庄市桥西区长兴街',
-        style: ''
-      },
-      {
-        id: 2,
-        name: '王五',
-        tel: 13552525252,
-        address: '河北省石家庄市裕华区槐安路',
-        style: ''
-      }
-    ],
-    startX: 0,
-
-  },
-  touchS: function(e) {
-    var that = this
-    // console.log(e)
-    if (e.touches.length == 1) {
-      that.setData({
-        startX: e.touches[0].clientX //开始触摸的x位置
-      })
-    }
-  },
-  touchM: function(e) {
-    var that = this
-    // console.log(e)
-    if (e.touches.length == 1) {
-      var moveX = e.touches[0].clientX //移动时的x位置
-      var disX = that.data.startX - moveX //移动的x距离差值
-      var delW = that.data.delWidth //删除按钮宽度
-      var style = ''
-      if (disX == 0 || disX < 0) {
-        style = "left:0px"
-      } else if (disX > 0 && disX < delW) {
-        style = "left:-" + disX + "px";
-      } else if (disX >= delW) {
-        style = "left:-" + delW + "px";
-      }
-    }
-
-    var idx = e.target.dataset.idx;
-    var list = that.data.list;
-    if (idx >= 0) {
-      list[idx].style = style
-      // console.log(style)
-      that.setData({
-        list: list
-      })
-    }
-  },
-  touchE: function(e) {
-    // console.log(e)
-    var that = this
-    if (e.changedTouches.length == 1) {
-      var endX = e.changedTouches[0].clientX;
-      var disX = that.data.startX - endX;
-      var delW = that.data.delWidth;
-
-      var style = disX > delW / 3 ? "left-" + delW + "px" : "left:0";
-      var idx = e.target.dataset.idx;
-      var list = that.data.list;
-      if (idx >= 0) {
-        list[idx].style = style
-        that.setData({
-          list: list
-        })
-      }
-    }
-  },
-  // 转换单位
-  getDelWidth: function(w) {
-    var real = 0;
-    try {
-      var res = wx.getSystemInfoSync().windowWidth;
-      var scale = (750 / 2) / (w / 2);
-      // console.log(scale);
-      real = Math.floor(res / scale);
-      return real;
-    } catch (e) {
-      return false;
-    }
-  },
-  initEleWidth: function() {
-    var delWidth = this.getDelWidth(this.data.delWidth);
-    this.setData({
-      delWidth: delWidth
-    })
-  },
-  delItem: function(e) {
-    var that = this
-    var idx = e.target.dataset.index;
-    var list = that.data.list
-    console.log(idx)
-    list.splice(idx, 1);
-    that.setData({
-      list: list
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    var that = this
-    that.initEleWidth();
+ data: {
+  items: [],
+  startX: 0, //开始坐标
+  startY: 0
+ },
+ onLoad: function () {
+  for (var i = 0; i < 10; i++) {
+   this.data.items.push({
+    content: i + " 向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦",
+    isTouchMove: false //默认全隐藏删除
+   })
   }
+  this.setData({
+   items: this.data.items
+  })
+ },
+ //手指触摸动作开始 记录起点X坐标
+ touchstart: function (e) {
+  //开始触摸时 重置所有删除
+  this.data.items.forEach(function (v, i) {
+   if (v.isTouchMove)//只操作为true的
+    v.isTouchMove = false;
+  })
+  this.setData({
+   startX: e.changedTouches[0].clientX,
+   startY: e.changedTouches[0].clientY,
+   items: this.data.items
+  })
+ },
+ //滑动事件处理
+ touchmove: function (e) {
+   console.log('e====>',e);
+   
+  var that = this,
+   index = e.currentTarget.dataset.index,//当前索引
+   startX = that.data.startX,//开始X坐标
+   startY = that.data.startY,//开始Y坐标
+   touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+   touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+   //获取滑动角度
+   angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+  that.data.items.forEach(function (v, i) {
+   v.isTouchMove = false
+   //滑动超过30度角 return
+   if (Math.abs(angle) > 30) return;
+   if (i == index) {
+    if (touchMoveX > startX) //右滑
+     v.isTouchMove = false
+    else //左滑
+     v.isTouchMove = true
+   }
+  })
+  //更新数据
+  that.setData({
+   items: that.data.items
+  })
+ },
+ /**
+  * 计算滑动角度
+  * @param {Object} start 起点坐标
+  * @param {Object} end 终点坐标
+  */
+ angle: function (start, end) {
+  var _X = end.X - start.X,
+   _Y = end.Y - start.Y
+  //返回角度 /Math.atan()返回数字的反正切值
+  return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+ },
+ //删除事件
+ del: function (e) {
+  this.data.items.splice(e.currentTarget.dataset.index, 1)
+  this.setData({
+   items: this.data.items
+  })
+ }
 })
